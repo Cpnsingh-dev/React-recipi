3a13de18cb78dd061c6a5747456d54c9053f518f
cpn <acpnsingh842323@gmail.com>
2026-04-26 22:24:22 +0530

    fixed RecipiCard image issue

--- src/Components/RecipiCard.jsx
+++ src/Components/RecipiCard.jsx
@@ -9,7 +9,11 @@ const RecipiCard = ({ meal }) => {
                 <div className='absolute insert-0 rounded-xl border-2 border-transparent group-hover:border-blue-500/100 transition'>
                 </div>
                 <div className='flex justify-center items-center p-5'>
-                    <img src={meal?.strMealThumb} alt="" className='h-60 w-60 rounded-xl border-yellow-400 transition duration-500 group-hover:scale-105' />
+                    <img
+                        src={meal?.strMealThumb}
+                        alt={meal?.strMeal}
+                        className='w-full h-48 object-cover rounded-xl transition duration-500 group-hover:scale-105'
+                    />
                 </div>
                 <div className='p-2 text-center '>
                     <h3 className='text-xl pb-3 font-bold text-gray-100 mb-1 group-hover:text-blue-400 transition duration-300'>{meal.strMeal}</h3>
